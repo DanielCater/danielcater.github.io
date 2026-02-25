e670415608e658f005db9e51f52dbaa0707ec3a6
Daniel Cater <d29cate@siena.edu>
2026-02-25 14:32:44 +0000

    Finished number guessing game

---
+++ game.js
@@ -0,0 +1,70 @@
+let randomNum = Math.floor(Math.random() * 100) + 1;
+
+const input = document.querySelector(".guessField");
+const button = document.querySelector(".guessSubmit");
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+let guessCount = 1;
+
+function evaluate(){
+    let num = Number(input.value);
+    input.value = "";
+
+    if(guessCount === 1){
+            guesses.textContent = "Previous guesses:";
+    }
+
+    guesses.textContent = `${guesses.textContent} ${num}`;
+
+    if(num === randomNum){
+        lastResult.textContent = "Congradulations, you guessed correct";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+        gameOver();
+    }
+    else if(guessCount === 10){
+        lastResult.textContent = "GAME OVER";
+        lowOrHi.textContent = "";
+        gameOver();
+    }
+    else{
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+
+        if(num < randomNum){
+            lowOrHi.textContent = "Last guess was too low";
+        }
+        else{
+            lowOrHi.textContent = "Last guess was too high";
+        }
+    }
+    guessCount++;
+}
+
+function gameOver(){
+    input.disabled = true;
+    button.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Start new game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", reset);
+}
+
+function reset(){
+    guessCount = 1;
+    guesses.textContent = "";
+    lastResult.textContent = "";
+    lowOrHi.textContent = "";
+    lastResult.style.backgroundColor = "white";
+    input.disabled = false;
+    button.disabled = false;
+    input.value = "";
+    input.focus();
+    resetButton.parentNode.removeChild(resetButton);
+
+    randomNum = Math.floor(Math.random() * 100) + 1;
+}
+
+button.addEventListener("click", evaluate);
