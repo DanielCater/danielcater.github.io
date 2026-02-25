e8af0abf31ec7386cdafa2608f9ef8e08a01e557
Daniel Cater <d29cate@siena.edu>
2026-02-25 13:13:40 +0000

    Added javascript practice

---
+++ myform.js
@@ -0,0 +1,13 @@
+//console.log("Hello world");
+//alert("Hello world");
+
+const buttonA = document.querySelector("#button_A");
+const headingA = document.getElementById("heading_A");
+
+buttonA.addEventListener("click", () => {
+    const name = prompt("What is your name?");
+    alert(`Hello ${name}, nice to see you`);
+    headingA.textContent = `Welcome, ${name}`;
+});
+
+
